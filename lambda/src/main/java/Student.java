b6ee3eb2d1201caa546619733fd0d1f5a07efd6b
FarukBatur <82152202+frkbtr@users.noreply.github.com>
2023-01-29 12:28:36 -0500

    implement lombok to the Student.java

--- lambda/src/main/java/Student.java
+++ lambda/src/main/java/Student.java
@@ -1,5 +1,13 @@
-public class Student {
+import lombok.*;
 
+//@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@RequiredArgsConstructor
+@Data
+public class Student {
     private String firstName;
     private String lastName;
     private String userName;
@@ -8,74 +16,14 @@ public class Student {
     private String confirmPassword;
     private int age;
 
+    //3 types of constructor: 1. no arg
+                            //2. all arg
+                            //3. required arg
 
-    public Student(){
-    }
+    // We can also make private constructor
 
 
-    public Student(String firstName, String lastName, String userName, String email, String password, String confirmPassword, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
-        this.age = age;
-    }
+    // @Data -> generate required arg cons, getter and setter, also toString
 
-    public String getFirstName() {
-        return firstName;
-    }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }

0f6f00e082e0d77a7c7ffb841990f8671579ede3
FarukBatur <82152202+frkbtr@users.noreply.github.com>
2023-02-01 22:14:58 -0500

    comparator interface review

--- lambda/src/main/java/compare_to_interface/CompareToDemo.java
+++ lambda/src/main/java/compare_to_interface/CompareToDemo.java
@@ -1,9 +1,8 @@
 package compare_to_interface;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+
+import static java.util.Comparator.comparing;
 
 public class CompareToDemo {
 
@@ -31,5 +30,40 @@ public class CompareToDemo {
         //Descending
         list.sort((o2,o1) -> o1.compareTo(o2));
         System.out.println(list);
+
+
+
+
+    List<Apple> myInventory = Arrays.asList(
+            new Apple(50, Color.GREEN),
+            new Apple(155,Color.RED),
+            new Apple(120, Color.GREEN)
+    );
+
+    //Comparing the apple based on weight
+    Comparator<Apple> sortApple = comparing((apple) -> apple.getWeight());
+    myInventory.sort(sortApple);
+    System.out.println(myInventory);
+
+    // import static method and implement double colon operator
+    myInventory.sort(comparing(apple -> apple.getWeight()));
+    myInventory.sort(comparing(Apple::getWeight));
+
+
+    //Reversed
+    myInventory.sort(comparing(Apple::getWeight).reversed());
+        System.out.println(myInventory);
+
+    //Chaining
+        myInventory
+                .sort(comparing(Apple::getColor)
+                        .reversed()
+                        .thenComparing(Apple::getWeight));
+        System.out.println(myInventory);
+
+
+
+
+
     }
 }

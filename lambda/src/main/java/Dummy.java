28cef9b3bfccc6bc66cc670146ef8cf70eddb6f7
Frank Batur <frank_batur@outlook.com>
2023-02-06 22:27:45 -0500

    dummmy commit

--- lambda/src/main/java/Dummy.java
+++ lambda/src/main/java/Dummy.java
@@ -3,5 +3,7 @@ public class Dummy {
     public static void main(String[] args) {
 
         System.out.println("Just a dummy class and dummy message for testing purpose ))");
+
+        System.out.println("after git config");
     }
 }

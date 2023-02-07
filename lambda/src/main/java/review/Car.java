612c5fb5c4ad26a4a5297466694c008d2cba59d5
FarukBatur <82152202+frkbtr@users.noreply.github.com>
2023-02-06 22:09:01 -0500

    review lombok

--- lambda/src/main/java/review/Car.java
+++ lambda/src/main/java/review/Car.java
@@ -1,47 +1,20 @@
 package review;
 
+import lombok.*;
+
+//@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@RequiredArgsConstructor
+@Data
+//@EqualsAndHashCode
 public class Car {
 
     private String make;
     private int topSpeed;
     private int year;
+ //   private final String color;
 
-    @Override
-    public String toString() {
-        return "Car{" +
-                "make='" + make + '\'' +
-                ", topSpeed=" + topSpeed +
-                ", year=" + year +
-                '}';
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public int getTopSpeed() {
-        return topSpeed;
-    }
-
-    public void setTopSpeed(int topSpeed) {
-        this.topSpeed = topSpeed;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public Car(String make, int topSpeed, int year) {
-        this.make = make;
-        this.topSpeed = topSpeed;
-        this.year = year;
-    }
 }

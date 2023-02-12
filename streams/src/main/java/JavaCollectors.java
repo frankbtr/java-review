b73e618463f0de30953077c770338f1e31ff2f3c
Frank Batur <frank_batur@outlook.com>
2023-02-12 17:10:34 -0500

    Collectors

--- streams/src/main/java/JavaCollectors.java
+++ streams/src/main/java/JavaCollectors.java
@@ -7,32 +7,38 @@ public class JavaCollectors {
 
         List<Integer> numbers = Arrays.asList(3,4,5,6,6,7);
         //toCollection(Supplier): is used to crate a collection using collector
-        System.out.println("--------toCollection----------");
+        System.out.println("--------toCollection()----------");
         List<Integer> numberList = numbers.stream()
                 .filter(x -> x%2 ==0)
                 .collect(Collectors.toCollection(ArrayList::new));
         System.out.println(numberList);
 
-        System.out.println("-----Set----");
+        System.out.println("-----Set()----");
         Set<Integer> numberSet = numbers.stream()
                 .filter(x -> x%2 ==0)
                 .collect(Collectors.toCollection(HashSet::new));
         System.out.println(numberSet);
 
-        //toList: returns a Collector interface that gathers the input data into a new List
+        //toList(): returns a Collector interface that gathers the input data into a new List
         // We will use toList a lot!
-        System.out.println("--------toList---------");
+        System.out.println("--------toList()---------");
         List<Integer> numberList2 = numbers.stream()
                 .filter(x -> x%2 == 0)
                 .collect(Collectors.toList());
         System.out.println(numberList2);
 
 
-        //toSet: returns a Collector interface that gathers the input data into a new Set
-        System.out.println("--------toSet---------");
+        //toSet(): returns a Collector interface that gathers the input data into a new Set
+        System.out.println("--------toSet()---------");
         Set<Integer> numberSet2 = numbers.stream()
                 .filter(x -> x%2 == 0)
                 .collect(Collectors.toSet());
         System.out.println(numberSet2);
+
+        //toMap(Function, Function): returns a Collector interface that gathers the input data into a new Map
+        System.out.println("--------toMap()---------");
+        Map<String, Integer> dishMap = DishData.getAll().stream()
+                .collect(Collectors.toMap(Dish::getName, Dish::getCalories));
+        System.out.println(dishMap);
     }
 }
